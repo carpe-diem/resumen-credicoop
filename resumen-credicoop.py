0e69e6b1a2f9c606ef61d41253374491b45f220e
Alberto Paparelli (a.k.a. carpediem) <alberto@paparelli.com.ar>
2013-06-11 00:43:01 -0300

    first commit

---
+++ resumen-credicoop.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import getopt
+import sys
+
+from src.process import ParseCredicoop
+from src.constants import TITLE, TITLE_PREV, TITLE_LAST
+
+
+HELP = """python credicoop.py -f <filename>"""
+
+
+def main(argv):
+    filename = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"h:f:",["filename="])
+
+    except getopt.GetoptError:
+        print HELP
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print HELP
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            filename = arg
+
+    if not filename:
+        print HELP
+
+    else:
+        try:
+            credicoop = ParseCredicoop(filename)
+            obj = credicoop.create()
+
+            print u"{0}{1}\n".format(
+                TITLE_PREV.rjust(20), obj.saldo_anterior.rjust(30))
+
+            print u"{0}{1}{2}{3}{4}{5}".format(
+                    TITLE[0].rjust(0),
+                    TITLE[1].rjust(20),
+                    TITLE[2].rjust(30),
+                    TITLE[3].rjust(20),
+                    TITLE[4].rjust(15),
+                    TITLE[5].rjust(10))
+
+            for x in obj.items:
+                "{}".rjust(4)
+                print u"{0}{1}{2}{3}{4}{5}".format(
+                    x.fecha.rjust(10),
+                    x.comprobante.rjust(10),
+                    x.descripcion.rjust(40),
+                    x.debito.rjust(15),
+                    x.credito.rjust(15),
+                    x.saldo.rjust(15))
+
+            print u"\n{0}{1}{2}".format(TITLE_LAST.rjust(20), obj.fecha_saldo.rjust(10),
+                    obj.saldo.rjust(20))
+
+        except Exception, e:
+            print e
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
---
+++ src/process.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from cStringIO import StringIO
+from lxml import etree
+
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import HTMLConverter
+from pdfminer.layout import LAParams
+
+from src.resume import Resumen, ResumenDetalle
+from src.constants import CODEC, TITLE, TITLE_PREV, TITLE_LAST
+
+
+class ParseCredicoop(object):
+
+    def __init__ (self, filename):
+        self.filename = filename
+        self.content = self.get_content()
+
+    def get_content(self):
+        doc = self.convert_pdf()
+        tree = etree.HTML(doc)
+        content = tree.xpath("/html/body/div[6]//text()")
+
+        new_line = []
+        for x in content:
+            line = x.strip()
+            if line:
+                new_line.append(line)
+
+        return new_line
+
+    def convert_pdf(self):
+
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = CODEC
+        laparams = LAParams()
+        device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+
+        fp = file(self.filename, 'rb')
+        process_pdf(rsrcmgr, device, fp)
+        fp.close()
+        device.close()
+
+        str = retstr.getvalue()
+        retstr.close()
+        return str
+
+
+    def create(self):
+        resumen = Resumen()
+
+        for line in self.content:
+
+            if all([x in TITLE for x in line.split()]):
+                continue
+
+            if TITLE_PREV in line:
+                saldo_anterior = line.strip().strip(TITLE_PREV)
+                saldo_anterior = saldo_anterior.replace('.', '')
+                saldo_anterior = saldo_anterior.replace(',', '.')
+                resumen.saldo_anterior = saldo_anterior
+
+            elif TITLE_LAST in line:
+                fecha, saldo = line.strip().strip(TITLE_LAST).split()
+                saldo = saldo.replace('.', '')
+                saldo = saldo.replace(',', '.')
+                resumen.saldo = saldo
+                resumen.fecha_saldo = fecha
+
+            else:
+                detalle = ResumenDetalle()
+
+                detalle.fecha = line[0:8]
+                detalle.comprobante = line[9:21].strip()
+                detalle.descripcion = line[21:50].strip()
+                detalle.debito = line[50:64].strip()
+                detalle.credito = line[64:98].strip()
+                detalle.saldo = line[85:].strip()
+
+                resumen.items.append(detalle)
+
+        return resumen
+
