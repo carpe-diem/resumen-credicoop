ac67d4dad4003ed5008471532c79bfd2b46f22f2
Alberto Paparelli (a.k.a. carpediem) <alberto@paparelli.com.ar>
2013-06-12 17:05:11 -0300

    licencias

--- credicoop/utils.py
+++ credicoop/utils.py
@@ -1,11 +1,29 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2013 Alberto Paparelli (a.k.a. carpediem)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  https://github.com/carpe-diem/resumen-credicoop/
+
+import re
+from unicodedata import normalize
+
 #
 #This snippet by Armin Ronacher can be used freely for anything you like.
 #Consider it public domain.
 #
-import re
-from unicodedata import normalize
-
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
@@ -18,3 +36,5 @@ def slugify(text, delim=u'-'):
         if word:
             result.append(word)
     return unicode(delim.join(result))
+
+# End snippet by Armin Ronacher
